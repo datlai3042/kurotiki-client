7e15c9bccd5825e06f85fdf12976928e3bd55f4a
datlai3042 <datlai304@gmail.com>
2025-04-15 14:32:53 +0700

    Đổi màu bg empty

--- src/component/Auth/AuthPermission.tsx
+++ src/component/Auth/AuthPermission.tsx
@@ -13,7 +13,7 @@ const AuthPermission = () => {
             <div className='bg-color-section-theme text-text-theme w-full h-full min-h-[80%] flex flex-col justify-center items-center text-[42px] font-bold gap-[20px]'>
                   <span className=' text-center'>Chức năng cần đăng nhập</span>
                   <button
-                        className='text-[14px] font-normal w-[150px] h-[45px] hover:bg-color-main hover:text-white border-[1px] bg-white border-slate-900 text-slate-900 rounded-md'
+                        className='text-[14px] font-normal w-[150px] h-[45px] hover:bg-color-main hover:text-white border-[1px] bg-white border-[var(--border-color-input)] hover:border-transparent text-slate-900 rounded-md'
                         onClick={handleAuth}
                   >
                         Đăng nhập
